532dc540526f637199fc9aa5c8f741bb35c68476
Kinantan <kinantanyolo@gmail.com>
2020-08-01 11:57:11 +0700

    Setting Homepage

---
+++ mockup-milestone/src/containers/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <h1>Tes</h1>
+            <p>Tes lagi</p>
+            <div>
+                <Link to="/infected-probability">
+                    <Button block bsSize="large">
+                        Kemungkinan Terinfeksi COVID-19
+                    </Button>
+                </Link>
+            </div>
+            <div>
+                <Link to="/symptoms">
+                    <Button block bsSize="large">
+                        Tingkat Kecocokan dengan Gejala COVID-19
+                    </Button>
+                </Link>
+            </div>
+        </div>
+    );
+}
