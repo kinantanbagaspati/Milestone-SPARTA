79b1a9b9fe4351a1463f040a75f5ee11811e6d52
kadeksuryam <kadeksuryam@gmail.com>
2020-08-11 01:11:41 +0700

    Updates

---
+++ precache-manifest.9377c85a114f04cb2f34be3a257045ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d760ca1e18b4b5e9747858867447ca1e",
+    "url": "/kadeksuryam/Tugas-Sparta-Covid.git/index.html"
+  },
+  {
+    "revision": "6b19aeeb108ce8041d9b",
+    "url": "/kadeksuryam/Tugas-Sparta-Covid.git/static/css/main.61e04c42.chunk.css"
+  },
+  {
+    "revision": "a85769ab36799056dc98",
+    "url": "/kadeksuryam/Tugas-Sparta-Covid.git/static/js/2.f723fee7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kadeksuryam/Tugas-Sparta-Covid.git/static/js/2.f723fee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b19aeeb108ce8041d9b",
+    "url": "/kadeksuryam/Tugas-Sparta-Covid.git/static/js/main.fc021546.chunk.js"
+  },
+  {
+    "revision": "dcd56e21cf6069b73f8c",
+    "url": "/kadeksuryam/Tugas-Sparta-Covid.git/static/js/runtime-main.d736961d.js"
+  },
+  {
+    "revision": "e3117830fd493812275af1a5993c4e84",
+    "url": "/kadeksuryam/Tugas-Sparta-Covid.git/static/media/Background.e3117830.jpg"
+  },
+  {
+    "revision": "5c7998e30bc00a13cdf944bc1295c27e",
+    "url": "/kadeksuryam/Tugas-Sparta-Covid.git/static/media/COVID.5c7998e3.jpg"
+  }
+]);
