8d1b718d0dc4d55538ba2384b84f7f38757cdad7
impper <impper1@gmail.com>
2020-08-10 21:18:05 +0700

    modif size dari background image

--- mockup-milestone/src/containers/Home.js
+++ mockup-milestone/src/containers/Home.js
@@ -10,27 +10,25 @@ export default class Home extends Component{
     render(){
         return (
             <div className="Container">
-                <div className="hero-image">
-                    <div className="header">
-                        <h1>Selamat datang di Website Pemantau Kemungkinan Terinfeksi COVID-19</h1>
-                    </div>
-                    <div className="kotakputihhome">
-                        <p className="pertanyaanhome">Apa yang ingin anda ketahui hari ini?</p>
-                        <div className="kumpulantombolhome">
-                            <div>
-                                <Link to="/infected-probability">
-                                    <Button className = "buttonHome" block bsSize="large">
-                                        <p className="tulisan">Kemungkinan Terinfeksi COVID-19</p>
-                                    </Button>
-                                </Link>
-                            </div>
-                            <div>
-                                <Link to="/symptoms">
-                                    <Button className="buttonHome" block bsSize="large">
-                                    <p className="tulisan">Tingkat Kecocokan dengan Gejala COVID-19</p>
-                                    </Button>
-                                </Link>
-                            </div>
+                <header className="header">
+                    <h1>Selamat datang di Website Pemantau Kemungkinan Terinfeksi COVID-19</h1>
+                </header>
+                <div className="kotakputihhome">
+                    <p className="pertanyaanhome">Apa yang ingin anda ketahui hari ini?</p>
+                    <div className="kumpulantombolhome">
+                        <div>
+                            <Link to="/infected-probability">
+                                <Button className = "buttonHome" block bsSize="large">
+                                    <p className="tulisan">Kemungkinan Terinfeksi COVID-19</p>
+                                </Button>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link to="/symptoms">
+                                <Button className="buttonHome" block bsSize="large">
+                                <p className="tulisan">Tingkat Kecocokan dengan Gejala COVID-19</p>
+                                </Button>
+                            </Link>
                         </div>
                     </div>
                 </div>
