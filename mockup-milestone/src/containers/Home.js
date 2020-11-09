646af160f0162d7601d69ee680e12abf5724fb09
kevinryann <kevinryanwijaya@gmail.com>
2020-11-09 22:28:58 +0700

    restore

---
+++ mockup-milestone/src/containers/Home.js
@@ -0,0 +1,47 @@
+import React, {Component} from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+
+export default class Home extends Component{
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return (
+            <div className="Container">
+                <div className="hero-image">
+                    <div className="header">
+                        <h1>Selamat datang di Website Pemantau Kemungkinan Terinfeksi COVID-19</h1>
+                    </div>
+                    <div className="kotakputihhome">
+                        <p className="pertanyaanhome">Apa yang ingin anda ketahui hari ini?</p>
+                        <div className="kumpulantombolhome">
+                            <div>
+                                <Link to="/infected-probability">
+                                    <Button className = "buttonHome" block bsSize="large">
+                                        <p className="tulisan">Kemungkinan Terinfeksi COVID-19</p>
+                                    </Button>
+                                </Link>
+                            </div>
+                            <div>
+                                <Link to="/symptoms">
+                                    <Button className="buttonHome" block bsSize="large">
+                                    <p className="tulisan">Tingkat Kecocokan dengan Gejala COVID-19</p>
+                                    </Button>
+                                </Link>
+                            </div>
+                            <div>
+                                <Link to="/algorithm">
+                                    <Button className="buttonHome" block bsSize="large">
+                                    <p className="tulisan">Algoritma yang dipakai di web ini mengenai COVID-19</p>
+                                    </Button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
